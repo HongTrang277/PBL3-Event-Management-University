bce0cde84178ba8277b06ee4445597d47639f600
HongTrang277 <ihongtrang277@gmail.com>
2025-05-03 14:52:57 +0700

    feat: Cài đặt các thư viện và môi trường cho front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
